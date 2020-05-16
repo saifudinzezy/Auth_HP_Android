770d8ffc0db466e62655340c1df241382b64b4fa
saefudin <udin@gmail.com>
2020-05-16 10:16:52 +0700

    OTP

---
+++ app/src/main/java/com/firebaseapp/authenthpfirebase/Home.java
@@ -0,0 +1,24 @@
+package com.firebaseapp.authenthpfirebase;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class Home extends AppCompatActivity {
+
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        textView.setText(""+getIntent().getStringExtra("phone"));
+    }
+}
---
+++ app/src/main/java/com/firebaseapp/authenthpfirebase/MainActivity.java
@@ -0,0 +1,64 @@
+package com.firebaseapp.authenthpfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener( v -> {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                    Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build())).build(), 1000);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                if (!FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber().isEmpty()) {
+                    startActivity(new Intent(this, Home.class).putExtra("phone", FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()));
+                    finish();
+                    return;
+
+                } else {
+                    if (response == null) {
+                        Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                        Toast.makeText(this, "No Network", Toast.LENGTH_SHORT).show();
+
+                        return;
+                    }
+
+                    if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                        Toast.makeText(this, "unkown Error", Toast.LENGTH_SHORT).show();
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
